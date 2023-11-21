3e7a31490f5e856f8f06caaa663b5cb02cc53fa1
SebaSalaG <ssebastianssalazar@gmail.com>
2023-11-21 17:11:02 -0300

    header
    boton

--- wp-content/themes/fcbh/header.php
+++ wp-content/themes/fcbh/header.php
@@ -29,8 +29,8 @@
 			<nav class="navbar navbar-expand-lg bg-transparent">
   <div class="container-fluid">
   <h1 class="navbar-brand"><?php the_custom_logo(); ?></h1>
-			    <button class="navbar-toggler bg-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+			    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<i class="fa-solid fa-dog"></i> / <i class="fa-solid fa-cat"></i>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav ms-auto mb-2 mb-lg-0 text-capitalize">
