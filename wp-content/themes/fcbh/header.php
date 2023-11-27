1338b400dcc7df3c788627f2d2d93e3e5beae179
SebaSalaG <ssebastianssalazar@gmail.com>
2023-11-26 23:16:29 -0300

    modal inicio de sesion
    modal no funcional de inicio de sesion

--- wp-content/themes/fcbh/header.php
+++ wp-content/themes/fcbh/header.php
@@ -47,11 +47,38 @@
 								<a class="nav-link" href="contacto">Contacto</a>
 								</li>
 								<li class="nav-item">
-								<a class="nav-link" href="#">Iniciar Sesión</a>
+								<a class="nav-link" data-bs-toggle="modal" data-bs-target="#inicio" href="#">Iniciar Sesión</a>
 								</li>
       </ul>
     </div>
   </div>
 </nav>
-		
-	</header><!-- #masthead -->
+
+	<div class="modal fade modal-info" id="inicio" tabindex="-1" aria-labelledby="inicioModalLabel" aria-hidden="true">
+			<div class="modal-dialog modal-dialog-centered modal-dialog-scrollabl modal-lg">
+				<div class="modal-content">
+					<div class="modal-body p-5 d-flex justify-content-center">
+						<form>
+							<label class="fw-bold" for="username">Nombre de Usuario:</label>
+							<input class="mb-5" type="text" id="username" name="username" placeholder="Ingrese su usuario" required>
+
+							<label class="fw-bold" for="password">Contraseña:</label>
+							<input class="mb-3" type="password" id="password" name="password" placeholder="Ingrese su contraseña" required>
+
+							<div class="d-flex justify-content-end mb-5">
+							<a href="#">¿Olvidó su contraseña?</a>
+							</div>
+
+							<div class="d-flex justify-content-center">
+								<button class="mb-4 px-5" type="submit">Iniciar Sesión</button>
+							</div>
+
+							<div class="d-flex justify-content-end mb-5">
+							<a href="#">¿Aún no tienes cuenta? <span class="fw-bold ps-3">Registrate</span></a>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+	</div>
+</header><!-- #masthead -->
