289dba190063a6586e20b7bba89a4965e3862950
jumaillard <ju.maillard@duocuc.cl>
2023-11-27 00:29:45 -0300

    cambios
    cambios julio pagina eventos creada falta hacerla dinamica

---
+++ wp-content/themes/fcbh/template-parts/content-proximos_eventos.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Template part for displaying page content in page.php
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package fundacioncbh
+ */
+
+?>
+
+<section class="my-5 mx-1">
+	<article class="eventos_card my-3">
+		<div class="container_info_fecha">
+			<span class="text_info_fecha">
+				13 <br> Octubre
+			</span>
+		</div>
+		<div class="container_info_eventos">
+			<h3>Jornada de adopción</h3>
+			<p>¡No te pierdas nuestro evento de adopción de mascotas este 13 de octubre en el Patio Outlet La Fábrica! Ven y encuentra a tu compañero peludo perfecto en un día lleno de amor y emoción. ¡Te esperamos!</p>
+			<span>Horario: 10 AM - 19 PM</span>
+		</div>
+      
+	</article>
+	<article class="eventos_card my-3">
+		<div class="container_info_fecha">
+			<span class="text_info_fecha">
+				13 <br> Octubre
+			</span>
+		</div>
+		<div class="container_info_eventos">
+			<h3>Jornada de adopción</h3>
+			<p>¡No te pierdas nuestro evento de adopción de mascotas este 13 de octubre en el Patio Outlet La Fábrica! Ven y encuentra a tu compañero peludo perfecto en un día lleno de amor y emoción. ¡Te esperamos!</p>
+			<span>Horario: 10 AM - 19 PM</span>
+		</div>
+      
+	</article>
+	<article class="eventos_card my-3">
+		<div class="container_info_fecha">
+			<span class="text_info_fecha">
+				13 <br> Octubre
+			</span>
+		</div>
+		<div class="container_info_eventos">
+			<h3>Jornada de adopción</h3>
+			<p>¡No te pierdas nuestro evento de adopción de mascotas este 13 de octubre en el Patio Outlet La Fábrica! Ven y encuentra a tu compañero peludo perfecto en un día lleno de amor y emoción. ¡Te esperamos!</p>
+			<span>Horario: 10 AM - 19 PM</span>
+		</div>
+      
+	</article>
+	<article class="eventos_card my-3">
+		<div class="container_info_fecha">
+			<span class="text_info_fecha">
+				13 <br> Octubre
+			</span>
+		</div>
+		<div class="container_info_eventos">
+			<h3>Jornada de adopción</h3>
+			<p>¡No te pierdas nuestro evento de adopción de mascotas este 13 de octubre en el Patio Outlet La Fábrica! Ven y encuentra a tu compañero peludo perfecto en un día lleno de amor y emoción. ¡Te esperamos!</p>
+			<span>Horario: 10 AM - 19 PM</span>
+		</div>
+      
+	</article>
+</section>
